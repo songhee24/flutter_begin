fce93741e50d42fe6c126bc6d03a2f38b34b9d23
songhee24 <akaratel61@gmail.com>
2022-09-03 23:28:31 +0600

    main setup

--- lib/main.dart
+++ lib/main.dart
@@ -12,39 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: const Color(0xFF1BB841),
-              centerTitle: false,
-              title: const Text('Сборы'),
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(
-                    Icons.phone,
-                    color: Colors.white,
-                  ),
-                  onPressed: () => {},
-                )
-              ],
-            ),
-            body: Container(
-              padding: const EdgeInsets.all(16),
-              child: Container(
-                  width: double.infinity,
-                  child: Container(
-                    height: 277,
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1BB841),
-                        borderRadius: const BorderRadius.only(
-                          topRight: Radius.circular(40),
-                          bottomLeft: Radius.circular(40),
-                        ),
-                        border: Border.all(
-                          width: 3.0,
-                          color: Colors.green,
-                          style: BorderStyle.solid,
-                        )),
-                    child: Center(child: const Text('Количество пользователей:')),
-                  )),
-            )));
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      body: const Text('Some text'),
+    ));
   }
 }
