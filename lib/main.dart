697f4a58f351dc4d05493d2ad5eb7553e7abbcea
songhee24 <akaratel61@gmail.com>
2022-09-04 00:24:21 +0600

    where have I seen it before...

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,33 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void answerQuestion() {
+    print('Answer Chosen !');
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      "What's your favorite color ?",
+      'What\s yout favorite animal ?'
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: const Text('My App'),
       ),
-      body: const Text('Some text'),
+      body: Column(
+        children: <Widget>[
+          const Text("The question!"),
+          ElevatedButton(
+              onPressed: () => answerQuestion(), child: const Text('Answer 1')),
+          ElevatedButton(
+              onPressed: answerQuestion, child: const Text('Answer 2')),
+          ElevatedButton(
+              onPressed: () => {print('Answer Chosen !')},
+              child: const Text('Answer 3')),
+        ],
+      ),
     ));
   }
 }
