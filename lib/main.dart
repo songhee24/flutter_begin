e520326b3bca2217350a0d6206a0d4a1bc23f1c8
songhee24 <akaratel61@gmail.com>
2022-09-03 23:18:08 +0600

    trying to create some widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  var myApp = MyApp();
+  const myApp = MyApp();
   runApp(myApp);
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello World'));
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color(0xFF1BB841),
+              centerTitle: false,
+              title: const Text('Сборы'),
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  onPressed: () => {},
+                )
+              ],
+            ),
+            body: Container(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                  width: double.infinity,
+                  child: Container(
+                    height: 277,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF1BB841),
+                        borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(40),
+                          bottomLeft: Radius.circular(40),
+                        ),
+                        border: Border.all(
+                          width: 3.0,
+                          color: Colors.green,
+                          style: BorderStyle.solid,
+                        )),
+                    child: Center(child: const Text('Количество пользователей:')),
+                  )),
+            )));
   }
 }
