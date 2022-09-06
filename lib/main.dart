0d867454073a8d5788b9275c332e73323921e78d
songhee24 <akaratel61@gmail.com>
2022-09-06 18:26:30 +0600

    trying to use if statements

--- lib/main.dart
+++ lib/main.dart
@@ -18,34 +18,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppWithState extends State<MyApp> {
+  final List<Map<String, Object>> questions = const [
+    {
+      "questionText": "What's your favorite color ?",
+      "answers": ["Black", "Red", "Green", "White"]
+    },
+    {
+      "questionText": 'What\s yout favorite animal ?',
+      "answers": ["Rabbit", "Snake", "Elephant", "Lion"]
+    },
+    {
+      "questionText": 'Who\s your favorite instructor ?',
+      "answers": ["Max", "Aza", "Sardor", "Zamir"]
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex += _questionIndex + 1;
     });
+
+    if(_questionIndex < questions.length) {
+      print('We have more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-   const List<Map<String, Object>> questions = [
-      {
-        "questionText": "What's your favorite color ?",
-        "answers": ["Black", "Red", "Green", "White"]
-      },
-      {
-        "questionText": 'What\s yout favorite animal ?',
-        "answers": ["Rabbit", "Snake", "Elephant", "Lion"]
-      },
-      {
-        "questionText": 'Who\s your favorite instructor ?',
-        "answers": ["Max", "Aza", "Sardor", "Zamir"]
-      },
-    ];
-
-
-    // if my value stored in some address has modifire const 
-    // we cant' add something into the value 
+    // if my value stored in some address has modifire const
+    // we cant' add something into the value
     // var dummy = const ['hello'];
     // dummy.add('aza');
     // print(dummy);
@@ -60,9 +62,13 @@ class _MyAppWithState extends State<MyApp> {
           Question(
             questionText: questions[_questionIndex]['questionText'] as String,
           ),
-         ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(selectHandler: _answerQuestion, answerText: answer,);
-         }).toList()
+          ...(questions[_questionIndex]['answers'] as List<String>)
+              .map((answer) {
+            return Answer(
+              selectHandler: _answerQuestion,
+              answerText: answer,
+            );
+          }).toList()
         ],
       ),
     ));
