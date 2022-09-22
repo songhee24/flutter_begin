85e43b64acb348b07759a92358bc090a7588dec2
songhee24 <akaratel61@gmail.com>
2022-09-22 22:23:36 +0600

    Getters & "else-if" logic

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final VoidCallback? selectHandler; 
+  final VoidCallback? selectHandler;
   final String? answerText;
 
   const Answer({super.key, this.selectHandler, this.answerText = 'alt text'});
--- lib/result.dart
+++ lib/result.dart
@@ -2,10 +2,32 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int? resultScore;
+
   const Result({super.key, this.resultScore});
+
+  String get resultPhrase {
+    String resultText = 'You did it!';
+
+    if (resultScore! <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore! <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore! <= 16) {
+      resultText = 'You are strange ?';
+    } else {
+      resultText = 'You are so bad !';
+    }
+
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('You did it!'));
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    ));
   }
 }
