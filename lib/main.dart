12bef40ba83e7040a22f20ec53fd57b938892c5b
songhee24 <akaratel61@gmail.com>
2022-09-24 16:33:07 +0600

    dart main project file structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,20 @@
 // below it
 // 2) Add a button which changes the text (to any other text of your choice)
 // 3) Split the app into three widgets: App, TextControl & Text
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key})
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
